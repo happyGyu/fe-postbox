c635b033e55ad64979e20c2ca976d277634c7545
연 <mogoo22@naver.com>
2022-03-11 16:10:17 +0900

    feat: 데모페이지 생성을 위해 json 경로를 깃허브로 변경

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 import {Town} from './object/Town.js';
 import {PostboxFinder} from './object/PostboxFinder.js';
 import {$} from './util/util.js';
-import {severURL} from './constants.js';
 
 function main() {
-  fetch(`${severURL}/info/random`)
+  fetch('https://raw.githubusercontent.com/happyGyu/fe-postbox/step2/randomInfo.json')
     .then(response => response.json())
     .then(init);
 }
